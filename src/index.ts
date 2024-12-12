dedc7f63c17538899e0c38e99b87832dd29f639e
Tron <achuan-2@outlook.com>
2024-12-12 22:07:26 +0800

    ✨ 支持去除换行、去除空格

--- src/index.ts
+++ src/index.ts
@@ -30,12 +30,48 @@ import { svelteDialog } from "./libs/dialog";
 const STORAGE_NAME = "config";
 
 export default class PluginSample extends Plugin {
-
+    private isMobile: boolean;
     private settingUtils: SettingUtils;
+    private data: { [key: string]: any } = {};
+    private topBarElement: HTMLElement;
 
     async onload() {
+        const frontEnd = getFrontend();
+        this.isMobile = frontEnd === "mobile" || frontEnd === "browser-mobile";
+        // 设置配置默认值
+        this.data[STORAGE_NAME] = {
+            latexConversion: false,
+            removeNewlines: false,
+            removeSpaces: false
+        }
+        await this.loadData(STORAGE_NAME);
+        console.log(this.data[STORAGE_NAME]);
+
         // 监听粘贴事件
-        this.eventBus.on("paste", this.eventBusPaste);
+        this.eventBus.on("paste", this.eventBusPaste.bind(this));
+        const topBarElement = this.addTopBar({
+            icon: "iconPaste",
+            title: this.i18n.addTopBarIcon,
+            position: "right",
+            callback: () => {
+                if (this.isMobile) {
+                    this.addMenu();
+                } else {
+                    let rect = topBarElement.getBoundingClientRect();
+                    // 如果被隐藏，则使用更多按钮
+                    if (rect.width === 0) {
+                        rect = document.querySelector("#barMore").getBoundingClientRect();
+                    }
+                    if (rect.width === 0) {
+                        rect = document.querySelector("#barPlugins").getBoundingClientRect();
+                    }
+                    this.addMenu(rect);
+                }
+            }
+        });
+        this.topBarElement = topBarElement;
+        // 更新顶栏按钮背景色
+        this.updateTopBarBackground();
     }
     
 
@@ -44,13 +80,13 @@ export default class PluginSample extends Plugin {
     }
 
     async onunload() {
-        this.eventBus.off("paste", this.eventBusPaste);
+        this.eventBus.off("paste", this.eventBusPaste.bind(this));
         console.log("onunload");
     }
 
 
     uninstall() {
-        this.eventBus.off("paste", this.eventBusPaste);
+        this.eventBus.off("paste", this.eventBusPaste.bind(this));
         console.log("uninstall");
     }
 
@@ -58,12 +94,80 @@ export default class PluginSample extends Plugin {
         // 如果需异步处理请调用 preventDefault， 否则会进行默认处理
         event.preventDefault();
         // 如果使用了 preventDefault，必须调用 resolve，否则程序会卡死
+        console.log(this.data[STORAGE_NAME])
         let text = event.detail.textPlain;
-        text = text.replace(/\\\[(.*?)\\\]/g, '$$$$$1$$$$'); // latex 行间数学公式块
-        text = text.replace(/\\\((.*?)\\\)/g, '$$$1$$'); // latex 行内数学公式
+        if (this.data[STORAGE_NAME].latexConversion) {
+            console.log()
+            text = text.replace(/\\\[(.*?)\\\]/g, '$$$$$1$$$$'); // latex 行间数学公式块
+            text = text.replace(/\\\((.*?)\\\)/g, '$$$1$$'); // latex 行内数学公式
+        }
+        if (this.data[STORAGE_NAME].removeNewlines) {
+            text = text.replace(/\n/g, ''); // 去除换行
+        }
+        if (this.data[STORAGE_NAME].removeSpaces) {
+            text = text.replace(/\s/g, ''); // 去除空格
+        }
         event.detail.resolve({
-            // 把laxtex公式变为markdown数学公式
             textPlain: text
         });
+    }
+
+    private addMenu(rect?: DOMRect) {
+        const menu = new Menu("pasteProcess", () => {});
+        menu.addItem({
+            icon: this.data[STORAGE_NAME].latexConversion ? "iconSelect" : "iconClose",
+            label: "Latex公式自动转换",
+            click: (detail, event) => {
+                event.preventDefault(); // Prevent menu from closing
+                this.toggleOption("latexConversion", detail);
+            }
+        });
+        menu.addItem({
+            icon: this.data[STORAGE_NAME].removeNewlines ? "iconSelect" : "iconClose",
+            label: "去除换行",
+            click: (detail, event) => {
+                event.preventDefault(); // Prevent menu from closing
+                this.toggleOption("removeNewlines", detail);
+            }
+        });
+        menu.addItem({
+            icon: this.data[STORAGE_NAME].removeSpaces ? "iconSelect" : "iconClose",
+            label: "去除空格",
+            click: (detail, event) => {
+                event.preventDefault(); // Prevent menu from closing
+                this.toggleOption("removeSpaces", detail);
+                
+            }
+        });
+        if (this.isMobile) {
+            menu.fullscreen();
+        } else {
+            menu.open({
+                x: rect.right,
+                y: rect.bottom,
+                isLeft: true,
+            });
+        }
+    }
+
+    private toggleOption(option: string, detail: any) {
+        this.data[STORAGE_NAME][option] = !this.data[STORAGE_NAME][option];
+        this.saveData(STORAGE_NAME, this.data[STORAGE_NAME]);
+        const useElement = detail.querySelector("use");
+        if (this.data[STORAGE_NAME][option]) {
+            useElement.setAttribute("xlink:href", "#iconSelect");
+        } else {
+            useElement.setAttribute("xlink:href", "#iconClose");
+        }
+        this.updateTopBarBackground();
+        console.log(this.data[STORAGE_NAME]);
+    }
+
+    private updateTopBarBackground() {
+        if (this.data[STORAGE_NAME].latexConversion || this.data[STORAGE_NAME].removeNewlines || this.data[STORAGE_NAME].removeSpaces) {
+            this.topBarElement.style.backgroundColor = "var(--b3-toolbar-hover)";
+        } else {
+            this.topBarElement.style.backgroundColor = "";
+        }
     }
 }
